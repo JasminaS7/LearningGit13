0ad524e6648e8f96fe3fb42c9b57d5ba4e7c4722
Jasmina Smileva <jasmina.smileva@gmail.com>
2022-06-20 17:48:39 +0200

    My Second Commit

---
+++ src/com/company/Class.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Class {
+
+
+        public static void main(String[] args) {
+
+            System.out.println("Learning GIT commands...");
+
+        }
+
+
+    }
